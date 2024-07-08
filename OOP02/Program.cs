9d911f7d8a95d56fae0f4d72c1528c0abcdfede2
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-08 23:05:36 +0300

    Solving Part02

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -25,6 +25,46 @@
             #endregion
 
 
+            //part02
+            #region Q6.Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
+
+            HireDate hireDate1 = new HireDate(27, 5, 2003);
+            HireDate hireDate2 = new HireDate(5, 7, 1979);
+            HireDate hireDate3 = new HireDate(1, 7, 2025);
+            Employee[] EmpArr = new Employee[3];
+            EmpArr[0] = new Employee(1, "Mahmoud Ibrahim", SecurityPrivilege.DBA, 90000m, hireDate1, Gender.m);
+            EmpArr[1] = new Employee(2, "Samir Hamed", SecurityPrivilege.Guest, 45000m, hireDate2, Gender.f);
+            EmpArr[2] = new Employee(3, "Aya mahmoud", SecurityPrivilege.SecurityOfficer, 120000m, hireDate3, Gender.f);
+
+
+            #endregion
+
+            #region 7.Sort the employees based on their hire date then Print the sorted array.
+            //While sorting(how many times Boxing and Unboxing process has occurred)
+
+            int boxing = 0;
+            int unboxing = 0;
+            for (int i = 0; i < EmpArr.Length - 1; i++)
+            {
+                for (int j = 0; j < EmpArr.Length; j++)
+                {
+                    boxing++;
+                    if (EmpArr[j].HireDate.CompareTo(EmpArr[j + 1].HireDate) > 0)
+                    {
+                        Employee temp = EmpArr[i];
+                        EmpArr[j] = EmpArr[j + 1];
+                        EmpArr[j + 1] = temp;
+
+                        unboxing += 3;
+                    }
+                }
+
+            }
+
+
+
+            #endregion
+
         }
     }
 }
